6c513cb4be90ebf0fcc1161a1fb0768419816d38
prasanjitsarker3 <sarkerprasanjit379@gmail.com>
2023-06-08 02:04:48 +0530

    Code User

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 //Middle Ware  
 app.use(cors())
 app.use(express.json())
@@ -25,6 +25,8 @@ async function run() {
         await client.connect();
         const instructorCollection = client.db("artCraftDB").collection("instructor");
         const classCollection = client.db("artCraftDB").collection("classes");
+        const cartCollection = client.db("artCraftDB").collection("carts");
+        const userCollection = client.db("artCraftDB").collection("users");
 
         //Instructor Information & Collection
         app.get('/instructor', async (req, res) => {
@@ -35,6 +37,61 @@ async function run() {
         app.get('/class', async (req, res) => {
             const result = await classCollection.find().toArray();
             res.send(result);
+        })
+
+        //Cart Add and delete Information 
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([])
+            }
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.post('/carts', async (req, res) => {
+            const item = req.body;
+            const result = await cartCollection.insertOne(item);
+            res.send(result)
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result)
+        })
+        //User Collection and Information
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existing = await userCollection.findOne(query)
+            if (existing) {
+                return res.send({ message: "User Already Exist" })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result);
+        })
+        app.patch("/users/admin/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result)
         })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
