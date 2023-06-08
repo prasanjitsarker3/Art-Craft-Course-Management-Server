ad8ccd89dbbc131605fee10adab75c10798550f1
prasanjitsarker3 <sarkerprasanjit379@gmail.com>
2023-06-08 21:15:44 +0530

    Push

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 //Middle Ware  
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    // console.log(authorization);
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'Unauthorized Access' })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESSTOKEN, (err, decoded) => {
+        console.log(err,decoded);
+        if (err) {
+            return res.status(403).send({ error: true, message: 'Unauthorized Access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ujahhqg.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,6 +38,7 @@ const client = new MongoClient(uri, {
     }
 });
 
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -28,6 +48,22 @@ async function run() {
         const cartCollection = client.db("artCraftDB").collection("carts");
         const userCollection = client.db("artCraftDB").collection("users");
 
+        //JWT 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESSTOKEN, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+        // const verifyAdmin = async (req, res, next) => {
+        //     const email = req.decoded.email;
+        //     const query = { email: email }
+        //     const user = await userCollection.findOne(query)
+        //     if (user?.role !== "admin") {
+        //         return res.status(403).send({ error: true, message: 'Forbidden Message' })
+        //     }
+        //     next()
+        // }
         //Instructor Information & Collection
         app.get('/instructor', async (req, res) => {
             const result = await instructorCollection.find().toArray();
@@ -40,10 +76,15 @@ async function run() {
         })
 
         //Cart Add and delete Information 
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
+           
             if (!email) {
                 res.send([])
+            }
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: "Unauthorized Access" })
             }
             const query = { email: email }
             const result = await cartCollection.find(query).toArray();
@@ -62,7 +103,9 @@ async function run() {
             res.send(result)
         })
         //User Collection and Information
-        app.get('/users', async (req, res) => {
+
+
+        app.get('/users', verifyJWT, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         })
@@ -82,6 +125,17 @@ async function run() {
             const result = await userCollection.deleteOne(query)
             res.send(result);
         })
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+
+        })
         app.patch("/users/admin/:id", async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
@@ -93,6 +147,9 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc)
             res.send(result)
         })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
